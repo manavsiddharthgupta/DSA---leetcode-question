a13e507a2ccc809c5ccd7fd3233846a0afac45b0
Manav Gupta <2006468@kiit.ac.in>
2022-02-13 22:06:17 +0530

    leetcode 2 question

---
+++ Arraysquestion4.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Arraysquestion4 {
+    //1637. Widest Vertical Area Between Two Points Containing No Points ---leetcode
+    public static void main(String[] args) {
+        int[][] points = {{3,1},{9,0},{1,0},{1,4},{5,3},{8,8}};
+        int max = maxWidthOfVerticalArea(points);
+        System.out.println(max);
+    }
+    public static int maxWidthOfVerticalArea(int[][] points) {
+        int[] allwidth = new int[points.length];
+        for (int i = 0; i < points.length; i++) {
+            allwidth[i] = points[i][0];
+        }
+        Arrays.sort(allwidth);
+        System.out.println(Arrays.toString(allwidth));
+        int max = 0;
+        for (int i = 1; i < allwidth.length; i++) {
+            if (max < allwidth[i] - allwidth[i-1]) {
+                max = allwidth[i] - allwidth[i-1];
+            }
+        }
+
+        return max;
+    }
+}
